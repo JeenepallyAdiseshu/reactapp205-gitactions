22972b559c10dcd6f9ccdf09935d7b9832a5b8b5
Adiseshu <jeenepallyadiseshu@gmail.com>
2025-07-29 12:37:43 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1>Welcome to My React Project - S205</h1>
       <p>This is a simple homepage deployed on GitHub Action using Vite.</p>
       <p>CI/CD and Cloud DevOps</p>
+      <p>Done with Git Pages & Git Actions</p>
     </div>
   );
 }
